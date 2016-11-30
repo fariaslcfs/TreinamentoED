f73808f7363e42df8f923ba8e0223621153d6b91
fariaslcfs <fariaslcfs@gmail.com>
2016-11-30 12:57:12 -0200

    geraCombMegaSena completo no eclipse OK

--- main.c
+++ main.c
@@ -269,11 +269,12 @@ int main(int argc, char *argv[]){
 
 	printf("O RESULTADO DE a X b EH %d\n", multIncremento(a, b));
 
-	geraCombMegaSena();
-
     */
 
-	arranjo(5);
+	geraCombMegaSena();
+
+
+//	arranjo(5);
 
 
 	return 0;
